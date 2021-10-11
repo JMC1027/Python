1fc127c7fa67ff2101cde12320cab86de9a28379
JMC1027 <87082922+JMC1027@users.noreply.github.com>
2021-10-11 11:19:23 -0400

    Create Inheritance.py

---
+++ Inheritance.py
@@ -0,0 +1,16 @@
+
+
+
+class School:
+    Name = 'Florida State University'
+    Location = 'Florida'
+    Phone = ''
+
+class Student:
+    GPA = '3.5'
+    Year = 'Senior'
+
+class Teacher:
+    Subject = 'History'
+    Email = 'Teacher.FS@school.com'
+    
