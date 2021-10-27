f39311fa3f223b45be17192e23c68000f5cf9a80
JMC1027 <87082922+JMC1027@users.noreply.github.com>
2021-10-27 10:44:59 -0400

    commit

--- MyVirtualEnviorments/HelloWorld/project1/src/mainapp/products/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Product(models.Model):
-    type = models.CharField(max_length=60)
-    name = models.CharField(max_length=60, default="", blank=True, null=False)
-    description = models.TextField(max_length=300, default="", blank=True)
-    price = models.DecimalField(default=0.00, max_digits=10000,decimal_places=2)
-    image = models.CharField(max_length=22, default='', blank=True)
--- MyVirtualEnviorments/HelloWorld/project1/src/mainapp/profiles/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Profiles
-
-admin.site.register(Profiles)
