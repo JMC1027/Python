bef4022ad3df26d7234b902890a740be42c797c4
JMC1027 <87082922+JMC1027@users.noreply.github.com>
2021-10-28 14:16:13 -0400

    django

--- MyVirtualEnviorments/Django_Checkbook/Django_Checkbook/Checkbook/views.py
+++ MyVirtualEnviorments/Django_Checkbook/Django_Checkbook/Checkbook/views.py
@@ -1,13 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Account, Transaction
+from .forms import AccountForm, TransactionForm
 
 def home(request):
-    return render(request, 'checkbook/index.html')
+    form = TransactionForm(data=request.POST or None)
+    if request.method == 'POST':
+        pk = request.POST['account']
+        return balance(request, pk)
+    content = {'form': form}
+    return render(request, 'checkbook/index.html', content)
 
 def create_account(request):
-    return render(request, 'checkbook/CreateNewAccount.html')
+    form = AccountForm(data=request.Post or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+        content = {'form': form}
+    return render(request, 'checkbook/CreateNewAccount.html', content)
 
-def balance(request):
-    return render(request, 'checkbook/BalanceSheet.html')
+def balance(request, pk):
+    account = get_object_or_404(Account, pk = pk)
+    transactions = Transaction.Transactions.filter(account = pk)
+    current_total = account.initial_deposit
+    table_contents = { }
+    for t in transactions:
+        if t.type == 'Deposit':
+            current_total += t.amount
+            table_contents.update({t: current_total})
+        else:
+            current_total -= t.amount
+            table_contents.update({t : current_total})
+    content = {'account':account}
+    return render(request, 'checkbook/BalanceSheet.html', content)
+
+
 
 def transaction(request):
-    return render(request, 'checkbook/AddTransaction.html')
+    form = TransactionForm(data=request.Post or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+        pk = request.POST['account']
+        form.save()
+        return balance(request, pk)
+
+
+
